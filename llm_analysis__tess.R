d70203b1f4c7ea9c1b28f50be15fd24d0b1467ff
Mark Andrews <mjandrews.org@gmail.com>
2025-06-08 18:14:36 +0100

    Update the llm analysis script

--- llm_analysis__tess.R
+++ llm_analysis__tess.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(ellmer)
 
+# LLM <- "Llama" # or LLM <- 'ChatGPT'
+LLM <- "ChatGPT"
+
 # helper functions -------------------------------------------------------
 
 chat <- function(post, instructions, llm = "Llama", database) {
@@ -12,19 +15,21 @@ chat <- function(post, instructions, llm = "Llama", database) {
   } else {
     stop("Unknown LLM")
   }
-  client$chat(format_post(post, database=database))
+  client$chat(format_post(post, database = database))
 }
 
-format_post <- function(post, database='allstat') {
-  
-  if (database == 'ncrm'){
+format_post <- function(post, database = "allstat") {
+  if (database == "ncrm") {
     title <- post$event
     description <- post$description
-  } else if (database == 'tess') {
+  } else if (database == "tess") {
     title <- post$name
     description <- post$description
+  } else if (database == "allstat") {
+    title <- post$title
+    description <- post$text
   }
-  
+
   str_c(
     str_c("# ", title, "\n\n"),
     description,
@@ -43,7 +48,7 @@ process_posts <- function(posts, instructions, llm, database, backup_file) {
     # We want to skip over any errors and not let them stop the flow
     tryCatch(
       {
-        results[[i]] <- chat(posts[[i]], instructions = instructions, llm = llm, database=database)
+        results[[i]] <- chat(posts[[i]], instructions = instructions, llm = llm, database = database)
       },
       error = function(e) {
         message("An error happened: ", e$message)
@@ -58,7 +63,7 @@ process_posts <- function(posts, instructions, llm, database, backup_file) {
   results
 }
 
-postprocess_results <- function(results, posts) {
+postprocess_results <- function(results, posts, database) {
   stopifnot(length(results) == length(posts))
   N <- length(results)
   processed_results <- vector(mode = "list", length = N)
@@ -66,15 +71,30 @@ postprocess_results <- function(results, posts) {
     processed_results[[i]] <- tryCatch(
       {
         result <- jsonlite::fromJSON(results[[i]], simplifyVector = TRUE)
-        result$date <- posts[[i]][['event-date']]
-        result$keywords2 <- unlist(posts[[i]][['Keywords']])
-        result$level2 <- posts[[i]][['Level']]
+        # the event date is specified in different ways in each database
+        # also, ncrm and tess posts have some extra attributes that we can store
+        if (database == "ncrm") {
+          result$date <- posts[[i]][["event-date"]]
+          result$keywords2 <- unlist(posts[[i]][["Keywords"]])
+          result$level2 <- posts[[i]][["Level"]]
+        } else if (database == "ncrm") {
+          result$date <- posts[[i]][["startDate"]]
+          result$keywords2 <- unlist(posts[[i]][["keywords"]])
+        } else if (database == "allstat") {
+          result$date <- posts[[i]][["month"]]
+        }
         result
       },
       error = function(e) NULL
     )
   }
-  processed_results
+  processed_results |>
+    discard(is.null) |>
+    # make each value a list to allow for concatenation
+    map(~ map(., list) |> as_tibble()) |>
+    bind_rows() |>
+    # unlist everything that can be unlisted
+    mutate(across(where(~ length(safe_unlist(.)) == length(.)), safe_unlist))
 }
 
 # this is used to unlist list-columns
@@ -95,34 +115,43 @@ read_json_bz2 <- function(json_bz2_path) {
   jsonlite::read_json(temp_file)
 }
 
+write_results <- function(results, results_df, database, llm) {
+  fname <- sprintf("data/%s_training_course_posts_%s_results.Rds", database, llm)
+  saveRDS(list(raw = results, data_frame = results_df), fname)
+}
 
 # Read in instructions and posts from file --------------------------------
 
+instructions <- readLines("instructions.md") |>
+  str_c(collapse = "\n")
+
+N <- 330
+
+allstat_posts <- read_json_bz2("data/allstat_training_course_posts.json.bz2")[1:N]
+ncrm_posts <- read_json_bz2("data/ncrm_events.json.bz2")[1:N]
+tess_posts <- read_json_bz2("data/tess_courses.json.bz2")[1:N]
 
 
-tess_posts <- read_json_bz2("data/tess_courses.json.bz2")
+# Process allstat posts ---------------------------------------------------
 
-# Process the posts ------------------------------------------------------
+allstat_results <- process_posts(allstat_posts, instructions = instructions, llm = LLM, database = "allstat", backup_file = "tmp/allstat_results_backup.Rds")
+allstat_results_df <- postprocess_results(allstat_results, allstat_posts, database = "allstat")
+write_results(allstat_results, allstat_results_df, "allstat", LLM)
 
-first_pass_instructions <- readLines("instructions2.md") |> str_c(collapse = "\n")
-results <- process_posts(tess_posts, instructions = first_pass_instructions, llm = "Llama", database='tess', backup_file = "tess_results_backup.Rds")
-tess_posts2 <- tess_posts[map_lgl(results, ~str_detect(., '^Yes.*'))]
+# Process TESS posts ------------------------------------------------------
 
-instructions <- readLines("instructions.md") |> str_c(collapse = "\n")
-results2 <- process_posts(tess_posts2, instructions = instructions, llm = "Llama", database='tess', backup_file = "tess_results_backup.Rds")
+# For TESS, we first filter out posts that do not appear at all related to statistics
+tess_first_pass_instructions <- readLines("tess_first_pass_instructions.md") |> str_c(collapse = "\n")
+first_pass_tess_results <- process_posts(tess_posts, instructions = tess_first_pass_instructions, llm = LLM, database = "tess", backup_file = "tmp/tess_first_pass_results_backup.Rds")
+tess_posts_filtered <- tess_posts[map_lgl(first_pass_tess_results, ~ str_detect(., "^Yes.*"))]
+
+tess_results <- process_posts(tess_posts, instructions = instructions, llm = LLM, database = "tess", backup_file = "tmp/tess_results_backup.Rds")
+tess_results_df <- postprocess_results(tess_results, tess_posts_filtered, database = "tess")
+write_results(tess_results, tess_results_df, "tess", LLM)
 
 
-# convert results to a data-frame
-# tricky because of the values that are lists
-results_df <- postprocess_results(results, tess_posts) |>
-  discard(is.null) |>
-  # make each value a list to allow for concatenation
-  map(~ map(., list) |> as_tibble()) |>
-  bind_rows() |>
-  # unlist everything that can be unlisted
-  mutate(across(where(~ length(safe_unlist(.)) == length(.)), safe_unlist))
+# Process NCRM ------------------------------------------------------------
 
-# Save results -----------------------------------------------------------
-
-# save raw results as-is and save the data frame
-saveRDS(list(raw = results, data_frame = results_df), "data/tess_training_course_posts_llama_results.Rds")
+ncrm_results <- process_posts(ncrm_posts, instructions = instructions, llm = LLM, database = "ncrm", backup_file = "tmp/results_backup.Rds")
+results_df <- postprocess_results(ncrm_results, ncrm_posts, database = "ncrm")
+write_results(ncrm_results, ncrm_results_df, "tess", LLM)
